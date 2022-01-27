4158923d3f02e70a5c7fd41cf611ef7a0d5f9597
Barbaross <57952969+Barbarossa93@users.noreply.github.com>
2022-01-27 13:21:34 -0500

    Delete .local/bin/old directory

--- .local/bin/old/greenclip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-greenclip print | sed '/^$/d' | dmenu -p Clipboard: | xargs -r -d'\n' -I '{}' greenclip print '{}'
--- .local/bin/old/link_grabber.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-regex='(((http|https|ftp|gopher)|mailto)[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
-url=$(grep -Po "$regex" | dmenu -p "Urls:") || exit
-linkhandler "$url"
