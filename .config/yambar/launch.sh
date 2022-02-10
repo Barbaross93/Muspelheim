4fa776768d245cf0703be18649af1b045a3ec679
Barbaross <cullenrss@gmail.com>
2022-02-10 15:56:50 -0500

    git version of yambar; overlines and fallback fonts

--- .config/yambar/launch.sh
+++ .config/yambar/launch.sh
@@ -6,10 +6,9 @@ pkill yambar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x yambar >/dev/null; do sleep 1; done
 
-setsid -f yambar -c ~/.config/yambar/border.yml
 setsid -f yambar
 
 sleep 2
-for i in $(xdo id -a yambar | tac); do
+for i in $(xdo id -a yambar); do
 	herbstclient lower "$i"
 done
