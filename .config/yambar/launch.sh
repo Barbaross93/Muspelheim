490718bccf3cdfcafae6b4b70d3f16355061996b
Barbaross <cullenrss@gmail.com>
2022-02-10 20:14:46 -0500

    minor yambar change

--- .config/yambar/launch.sh
+++ .config/yambar/launch.sh
@@ -7,6 +7,7 @@ pkill yambar
 while pgrep -u $UID -x yambar >/dev/null; do sleep 1; done
 
 setsid -f yambar
+setsid -f yambar -c ~/.config/yambar/ext-config.yml
 
 sleep 2
 for i in $(xdo id -a yambar); do
