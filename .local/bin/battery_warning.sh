24169bba6cf372055bd178acff2a6982c3f8ee60
Barbaross <cullenrss@gmail.com>
2022-04-19 12:49:09 -0400

    misc changes

--- .local/bin/battery_warning.sh
+++ .local/bin/battery_warning.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
-#battery_level=$(acpi -b | awk NR==1 | grep -P -o '[0-9]+(?=%)')
-if [ "$battery_level" -le 5 ]; then
+battery_status=$(cat /sys/class/power_supply/BAT0/status)
+if [ "$battery_level" -le 5 ] && [ "$battery_status" != "Charging" ]; then
 	sudo ZZZ
-elif [ "$battery_level" -le 10 ]; then
+elif [ "$battery_level" -le 10 ] && [ "$battery_status" != "Charging" ]; then
 	notify-send -u critical -t 5000 "Battery low" "Battery level is ${battery_level}%!"
 elif [ "$battery_level" -ge 100 ]; then
 	notify-send -t 10000 "Battery Full" "Disconnect the power"
 fi
 
 bt_bat_check=$(find /sys/class/power_supply/* | wc -l)
-#bt_bat_check=$(acpi -b | wc -l)
-if [ $bt_bat_check -eq 3 ]; then
+if [ "$bt_bat_check" -eq 3 ]; then
 	bt_bat_lvl=$(cat /sys/class/power_supply/hid-34:88:5d:b6:e9:13-battery/capacity)
 	if [ "$bt_bat_lvl" -le 10 ]; then
 		notify-send -u critical -t 5000 "Battery low" "Bluetooth battery level is low! ${bt_bat_lvl}% remaining!"
--- .local/bin/kb_variant.sh
+++ .local/bin/kb_variant.sh
@@ -4,12 +4,12 @@
 
 if setxkbmap -query | grep variant >/dev/null; then
 	setxkbmap us
-	#xmodmap ~/.config/x11/Xmodmap
-	#sv restart ~/.local/service/xcape
+	xmodmap ~/.config/x11/Xmodmap
+	sv restart ~/.local/service/xcape
 	#notify-send "Keymap" "Keymap set to qwerty"
 else
 	setxkbmap us -variant colemak -option caps:capslock
-	#xmodmap ~/.config/x11/Xmodmap
-	#sv restart ~/.local/service/xcape
+	xmodmap ~/.config/x11/Xmodmap
+	sv restart ~/.local/service/xcape
 	#notify-send "Keymap" "Keymap set to colemak"
 fi
